f2d8d3669da0d609d1f10be82ab0b30a967ad13d
Sajid <sajid.abdullah10@gmail.com>
2024-11-19 12:21:14 -0500

    Defined CSS variables and adding attributes

--- src/Pages/Home/Hero_Section.jsx
+++ src/Pages/Home/Hero_Section.jsx
@@ -4,8 +4,21 @@ export default function HeroSection() {
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
                     <p className="section--title">Hey, I'm in the hero section</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section--title--color">Hero Section but..</span>{" "}
+                        <br />
+                        developer
+                    </h1>
+                    <p className="hero--section-description">I'm the hero section description!
+                        <br /> description's break tag
+                    </p>
                 </div>
+                <button className="btn btn-primary">Get In Touch</button>
             </div>
+        <div className="hero--section--img">
+            <img src="./img/hero_img.png" alt="Hero Section"></img>
+        </div>
+
         </section>
     )
 }
