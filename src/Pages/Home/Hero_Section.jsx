387142ea1c76a866c0874a3ab0ff7b45bb0cb5a5
Sajid <sajid.abdullah10@gmail.com>
2025-01-22 08:50:24 -0500

    fix herosection text

--- src/Pages/Home/Hero_Section.jsx
+++ src/Pages/Home/Hero_Section.jsx
@@ -4,14 +4,14 @@ export default function HeroSection() {
         <section id="heroSection" className="hero--section">
             <div className="hero--section--content--box">
                 <div className="hero--section--content">
-                    <p className="section--title">Hey, I'm in the hero lol</p>
+                    <p className="section--title">Hey, I'm Sajid</p>
                     <h1 className="hero--section--title">
-                        <span className="hero--section--title--color">Hero Section but..</span>{" "}
+                        <span className="hero--section--title--color">Computer Science</span>{" "}
                         <br />
-                        developer
+                        Student
                     </h1>
-                    <p className="hero--section-description">I'm the hero section description!
-                        <br /> description's break tag
+                    <p className="hero--section-description">Solving Problems with Code | Software Enthusiast
+                        {/*<br /> description's break tag*/}
                     </p>
                 </div>
                 <button className="btn btn-primary">Get In Touch</button>
