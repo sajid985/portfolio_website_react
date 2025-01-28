e0a48529cdb40ee40f8a4d33e371a09a35b5589c
Sajid <sajid.abdullah10@gmail.com>
2025-01-28 11:50:03 -0500

    fix contactme form

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../Hero_Section";
+import Contact_Me from "../Contact_Me";
 
 export default function Home () {
     return (
         //react fragment (placeholder)
         <>
             <HeroSection />
+            <Contact_Me />
         </>
-    )
+    );
 }
